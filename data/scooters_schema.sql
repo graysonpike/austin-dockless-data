5fdae950a9e34e080ac1ce9fee0e61e4ff06fa49
Grayson Pike <graysonpike@gmail.com>
2019-04-22 22:46:55 -0500

    Added scooters db and schema

---
+++ data/scooters_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `scooters` (
+	`Device ID`	TEXT NOT NULL,
+	`Number of Trips`	INTEGER NOT NULL,
+	`Total Duration of Trips (min)`	INTEGER NOT NULL,
+	`Earilest Ride Timestamp`	TEXT NOT NULL,
+	`Last Ride Timestamp`	TEXT NOT NULL,
+	`Lifespan (days)`	INTEGER NOT NULL,
+	`Total Distance (m)`	INTEGER NOT NULL,
+	`Estimated Revenue`	INTEGER NOT NULL
+);
